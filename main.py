6ef734057ddfe02ded5ef417640e00d906edb6db
Unknown <ponur0kirill@gmail.com>
2019-04-08 22:48:08 +0300

    Это был очень кривой коммит

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ import sys
 from scipy import interpolate
 from pandas import read_excel as read
 
+def frexp10(decimal):
+	parts = ('%e' % decimal).split('e')
+	return float(parts[0]), int(parts[1])
+
 sheet_name='5.2'
 rec = path.abspath('.\\rec\\rec.xlsx')
 df=read(rec, sheet_name=sheet_name)
@@ -21,16 +25,103 @@ g = polyfit(x,y,9)
 error=poly1d(g)
 l,b,d= 22e-3, 0.38e-3 , 3.5e-3
 
-rho=resistance*l/(d*b)
-sigma=(1/rho)%10
-R1=2.18*10**(-11)
-R2=2.12*10**(-11)
-R4=2.03*10**(-11)
-R7=2.89*10**(-11)
-R200= 1.88*10**(-11)
-R400= 2.18*10**(-11)
-R600= 1.93*10**(-11)
-R900= 1.93*10**(-11)
+rho=resistance*(d*b)/l
+print('Удельное сопротивление',rho)
+#Проверил по таблицам вроде порядок тот же
+sigma=(1/rho)
+
+sheet_name='5.3'
+rec = path.abspath('.\\rec\\rec.xlsx')
+df=read(rec, sheet_name=sheet_name)
+x=array(df['I']) # В Амперах
+y=array(df['U'])
+g = polyfit(x,y,9)
+error=poly1d(g)
+print(error(10))
+
+
+df=read(rec, sheet_name='5.4, I=1mA')
+x=array(df['I'])*773/10e4 # В Тл
+y=array(df['E']) # В Вольтах
+g = polyfit(x,y,1)
+R1=g[0]*b/(1*10e-3)
+print('Для I=1mA',R1)
+
+
+
+
+df=read(rec, sheet_name='5.4, I=2mA')
+x=array(df['I'])*773/10e4 # В Тл
+y=array(df['E']) # В Вольтах
+g = polyfit(x,y,1)
+R2=g[0]*b/(2*10e-3)
+print('Для I=2mA',R2)
+
+
+
+df=read(rec, sheet_name='5.4, I=4mA')
+x=array(df['I'])*773/10e4 # В Тл
+y=array(df['E']) # В Вольтах
+g = polyfit(x,y,1)
+R4=g[0]*b/(4*10e-3)
+print('Для I=4mA',R4)
+
+
+
+
+df=read(rec, sheet_name='5.4, I=7mA')
+x=array(df['I'])*773/10e4 # В Тл
+y=array(df['E']) # В Вольтах
+g = polyfit(x,y,1)
+R7=g[0]*b/(7*10e-3)
+print('Для I=7mA',R7)
+
+
+
+sheet_name='5.6, B=200'
+df=read(rec, sheet_name=sheet_name)
+x=array(df['I'])/1000 # В Амперах 
+y=array(df['E']) # В Вольтах
+g = polyfit(x,y,1,w=1/error(x))
+y=poly1d(g)
+R200=(g[0]*b/200e-4)
+print('Для B=200Гс',R200)
+
+
+
+
+sheet_name='5.6, B=400'
+df=read(rec, sheet_name=sheet_name)
+x=array(df['I1']) /1000
+y=array(df['E1'])
+g = polyfit(x,y,1,w=1/error(x))
+y=poly1d(g)
+R400=(g[0]*b/400e-4)
+print('Для B=400 Гс',R400)
+
+
+
+sheet_name='5.6, B=600'
+df=read(rec, sheet_name=sheet_name)
+x=array(df['I'])/1000 # В Амперах
+y=array(df['E'])
+g = polyfit(x,y,1,w=1/error(x))
+y=poly1d(g)
+R600=(g[0]*b/600e-4)
+print('Для B=600 Гс',R600)
+
+
+
+sheet_name='5.6, B=900'
+df=read(rec, sheet_name=sheet_name)
+x=array(df['I'])/1000 # В Амперах
+y=array(df['E'])
+g = polyfit(x,y,1,w=1/error(x))
+y=poly1d(g)
+R900=(g[0]*b/900e-4)
+print('Для B=900 Гс',R900)
+
+
 
 R=array([R1,R2,R4,R7,R200,R400,R600,R900])
 mu=mean(sigma*R)
@@ -38,3 +129,7 @@ mu=mean(sigma*R)
 A=3*pi/8
 e=1.60217662e-19
 n=A/mean(R)/e
+
+print('Концентрация=',n)
+print('Подвижность=',mu)
+print('Проводимость=',sigma)
