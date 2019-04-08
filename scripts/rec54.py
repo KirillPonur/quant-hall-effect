40dab8d18cafc09ca69041e7ba2aa049d6686ea7
Unknown <ponur0kirill@gmail.com>
2019-04-08 23:17:05 +0300

    Правки

--- scripts/rec54.py
+++ scripts/rec54.py
@@ -30,7 +30,7 @@ rec = path.abspath('..'+'\\rec\\rec.xlsx')
 df=read(rec, sheet_name='5.4, I=1mA')
 
 # figure('5.4, I=1mA')
-x=array(df['I'])*773*10e4 # В Тл
+x=array(df['I'])*773/10e4 # В Тл
 y=array(df['E']) # В Вольтах
 g = polyfit(x,y,1)
 print('Для I=1mA',g[0]*b/(1*10e-3))
@@ -38,7 +38,7 @@ y=poly1d(g)
 x=linspace(0,x[-1],1000)
 
 plot(x,y(x),label=r'$I=1\text{ мА}$')
-plot(df['I']*773*10e4,df['E'],'r.',label='_nolegend_')
+plot(df['I']*773/10e4,df['E'],'r.',label='_nolegend_')
 
 # legend()
 # savefig(path.abspath('..'+'\\fig\\541.pdf'))
@@ -48,7 +48,7 @@ plot(df['I']*773*10e4,df['E'],'r.',label='_nolegend_')
 
 df=read(rec, sheet_name='5.4, I=4mA')
 # figure('5.4, I=2mA')
-x=array(df['I'])*773*10e4 # В Тл
+x=array(df['I'])*773/10e4 # В Тл
 y=array(df['E']) # В Вольтах
 g = polyfit(x,y,1)
 print('Для I=4mA',g[0]*b/(4*10e-3))
@@ -56,14 +56,14 @@ y=poly1d(g)
 x=linspace(0,x[-1],1000)
 
 plot(x,y(x),label=r'$I=4\text{ мА}$')
-plot(773*df['I']*10e4,df['E'],'r.',label='_nolegend_')
+plot(773*df['I']/10e4,df['E'],'r.',label='_nolegend_')
 
 # legend()
 # savefig(path.abspath('..'+'\\fig\\542.pdf'))
 
 df=read(rec, sheet_name='5.4, I=7mA')
 # figure('5.4, I=7mA')
-x=array(df['I'])*773*10e4 # В Тл
+x=array(df['I'])*773/10e4 # В Тл
 y=array(df['E']) # В Вольтах
 g = polyfit(x,y,1)
 print('Для I=7mA',g[0]*b/(7*10e-3))
@@ -71,7 +71,7 @@ y=poly1d(g)
 x=linspace(0,x[-1],1000)
 
 plot(x,y(x),label=r'$I=\text{7 мА}$')
-plot(773*df['I']*10e4,df['E'],'r.',label='_nolegend_')
+plot(773*df['I']/10e4,df['E'],'r.',label='_nolegend_')
 ylabel(r'$\mathscr{E}_H$, \text{В}',fontsize=16)
 xlabel(r'$B, \text{Тл}$',fontsize=16)
 grid(which='major', linestyle='-')
@@ -81,7 +81,7 @@ minorticks_on()
 
 df=read(rec, sheet_name='5.4, I=2mA')
 # figure('5.4, I=2mA')
-x=array(df['I'])*773*10e4 # В Тл
+x=array(df['I'])*773/10e4 # В Тл
 y=array(df['E']) # В Вольтах
 g = polyfit(x,y,1)
 print('Для I=2mA',g[0]*b/(2*10e-3))
@@ -89,7 +89,7 @@ y=poly1d(g)
 x=linspace(0,x[-1],1000)
 
 plot(x,y(x),label=r'$I=2\text{ мА}$')
-plot(773*df['I']*10e4,df['E'],'r.',label='_nolegend_')
+plot(773*df['I']/10e4,df['E'],'r.',label='_nolegend_')
 
 
 legend()
